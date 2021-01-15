f32e3df0728a3da9b297486b3d8700f3a83ca08a
TBchip <bischoffthijs@gmail.com>
2021-01-15 11:14:46 +0100

    update

---
+++ cppWebcam/src/mainProgram.cpp
@@ -0,0 +1,47 @@
+#include "mainProgram.h"
+
+using namespace cv;
+
+
+void sendFrame(Mat frame) {
+	std::vector<uchar> buf;
+	imencode(".jpg", frame, buf);
+
+	std::string bufStr(buf.begin(), buf.end());
+
+	sendData(bufStr);
+}
+
+void webcamLoop(int fps) {
+	try {
+		Mat frame;
+
+		frame = getWebcamFrame();
+
+		sendFrame(frame);
+		//imshow("Webcam", frame);
+
+		if (waitKey(1000 / fps) >= 0)
+			return;
+	}
+	catch (std::exception& e)
+	{
+		std::cerr << "Exception: " << e.what() << std::endl;
+	}
+}
+
+int mainProgram(HANDLE stopEvent) {
+	ShowWindow(GetConsoleWindow(), SW_HIDE);
+
+	if (!isWebcamSetupCorrectly())
+		return 1;
+
+	const int webcamFps = 30;
+
+	// check if service should stop
+	while (WaitForSingleObject(stopEvent, 0) != WAIT_OBJECT_0) {
+		webcamLoop(webcamFps);
+	}
+
+	return 0;
+}
---
+++ cppWebcam/src/mainProgram.h
@@ -0,0 +1,11 @@
+#pragma once
+
+#include <iostream>
+#include <vector>
+#include <bitset>
+
+#include "webcam.h"
+#include "networking.h"
+
+int mainProgram(HANDLE stopEvent);
+
